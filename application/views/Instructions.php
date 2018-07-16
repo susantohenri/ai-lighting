288374568e9c009dc7cf0b1df555ec259506b2b5
ari yulianto <sadhean@gmail.com>
2018-07-16 09:25:50 +0700

    process instructions

--- application/views/Instructions.php
+++ application/views/Instructions.php
@@ -1,9 +1,351 @@
 <div class="panel-body">
   <div class="row">
-    <div class="col-sm-12">
+    <div class="col-sm-12 text-center">
         <h3 class="text-center" style="margin:5px 0px;"><strong>Process Instrutions</strong></h3> <br>
-        </div>
+        <a href="<?= base_url('asset/file/process_instruction_rev1.pdf') ?>" target="_blank" class="btn btn-md btn-success text-center">Download Process Instructions</a>
     </div>
   </div>
+  <br>
+  <div class="row" >
+    <div class="col-md-offset-1 col-md-10" style="border-style:outset;">
+      <br>
+      <div class="row">
+        <div class="col-xs-offset-1 col-xs-6">
+          <h4 class="bold">LED Highbay VEET Upgrade Process</h4>      
+        </div>
+        <div class="col-xs-3 pull-right">
+          <img src="<?= base_url('asset/img/ai-lighting-150.png') ?>" style="width:80px;padding:5px" alt="" class="pull-right">
+        </div>
+      </div>
+      <br>
+      <br>
+      <div class="row">
+        <div class="col-sm-offset-1 col-sm-10">
+          <p class="text-justify">
+            The evidence collection and paperwork component of a VEET lighting upgrade is not as complicated as many seem to think. If you spend the time to understand the process and follow these guidelines, it will add less than 20 minutes to the average Project.
+          </p>
+          <p class="text-justify">
+            The documentation is broken down into the following components
+          </p>
+          <ul>
+            <li><p class="bold">Assignment Form and Data Collection Sheet</p></li>
+            <li><p class="bold">Customer Invoice and Certificate of Electrical Safety</p></li>
+            <li><p class="bold">BCA Evidence</p></li>
+            <li><p class="bold">Photographic Evidence</p></li>
+            <li><p class="bold">Floor Plans</p></li>
+          </ul>
+          <p class="text-justify">
+            This instructional document will go through each point. At the end of the document will be an example of a completed job for reference. For quick reference you may wish to print this document, or you can save this pdf document to your mobile device in iBook’s or an equivalent Android document viewer.
+          </p>
+          <br>
+          <p class="bold">Assignment Form and Data Collection Sheet</p>
+          <p class="text-justify">
+            This document is to be completed by hand from site. Please print text and ensure it is clearly readable. This is a self-explanatory step and can be mostly completed by the customer them selves while you are working. The check list provided should be marked off progressively to not miss an evidence step. e.g. Forgetting the baseline photos before the fittings are removed can invalidate the VEET claim altogether.
+          </p>
+          <br>
+          <p class="bold">
+            Customer Invoice and Certificate of Electrical Safety
+          </p>
+          <p class="text-justify">
+            These two items are incidental with any electrical job and will generally occur off site. VEET requirements mandate very particular details to be included on both documents. If they are missed, the claim will be held up until the correct amendments are made and the documents are resubmitted.
+          </p>
+          <p>
+            Key details that are usually not required for average jobs are
+          </p>
+          <ul>
+            <li>
+              <p>Customers ABN to be listed on the invoice under the recipient details.</p>
+            </li>
+            <li>
+              <p>Make and model of the upgrade lighting to be detailed on the CES</p>
+            </li>
+            <li>
+              <p>Details of any control device added on both invoice ad CES</p>
+            </li>
+            <li>
+              <p>Accurate completion date of the project on the CES</p>
+            </li>
+            <li>
+              <p>VEET Discount provided to customer</p>
+            </li>
+          </ul>
+          <p class="text-justify">
+            The VEET discount is the agreed ‘Installed Payback Price’ multiplied by the number of fittings upgraded as a part of the project. If the revenue generated is enough to do the job as a zero out of pocket cost to the customer, the invoice should display a $0.00 total
+          </p>
+          <p class="text-justify">
+            If the revenue provides a discount only and a co payment is required, the balance amount needs to be visible on the invoice and becomes a functional invoice that the customer will be required to settle.
+          </p>
+          <br>
+          <br>
+        </div>
+      </div>
+    </div>
+  </div>
+<br>
+
+  <div class="row">
+    <div class="col-md-offset-1 col-md-10" style="border-style:outset;">
+      <br>
+      <div class="row">
+        <div class="col-sm-offset-1 col-sm-10">
+          <p class="text-justify">
+            The purpose of this is for the customer to understand the value of their discount.
+            For the first few projects ai will provide you the exact terminology and help you though this process.
+          </p>
+          <br>
+          <p class="bold">BCA Evidence</p>
+          <p class="text-justify">
+            The BCA evidence is incredibly important and unless it is correct, the whole claim can fall over or be devalued depending on the evaluated use of the building.
+          </p>
+          <p class="text-justify">
+            Firstly, not all high bay upgrades are valued equally. Different BCA classifications of the premises determine what the estimated annual operating hours are. There are three main categories that are relevant to high bay installations. 2000, 3000 and 5000 hours.
+          </p>
+          <p class="text-justify">
+            You are not expected to be an expert in this assessment, ai lighting will work with you to determine this. In the ‘High bays and Pricing’ page, there is an ‘Application’ column that will cover off most of the possible installations.
+          </p>
+          <p class="text-justify">
+            The BCA evidence collection can be challenging, and it is advised to have this completed before agreeing to or undertaking the installation.
+          </p>
+          <p class="text-justify">
+            The most comprehensive evidence that covers off all the annual operating hours categories is a rental lease. The rental lease will contain the permitted use which is accepted by the regulator.
+          </p>
+          <p class="text-justify">
+            For retail businesses we can accept a dedicated web page or photos of the shop front that clearly show the stores opening hours. This will only be accepted if it is reviewed and approved by ai lighting prior to the job.
+          </p>
+          <br>
+          <p class="bold">
+            Photographic Evidence
+          </p>
+          <p class="text-justify">
+            The single most important part of the photographic evidence is ensuring that your smartphone’s settings are enabled to allow location data to be stored in the photos. This is called geotagging and the data in the photo is called meta data. Please check your settings and test to ensure that every photo contains date/ time stamp and GPS coordinates. This is a mandatory requirement and can cause a claim to fail if not available. Ai lighting will work with you to transfer the photos ensuring the data remains intact.
+          </p>
+          <p class="text-justify">
+            Before and after photos are required from each upgrade space, showing as many of the luminaires as possible.
+Each Luminaire, Lamp, Ballast and control device must be <b>photographed from the installation site</b> clearly displaying all the text on each item.
+          </p>
+          <p class="text-justify">
+            A Photo of the removed lighting in a pile with all power leads cut off must be provided to prove decommissioning.
+          </p>
+          <br>
+          <p class="bold">Floor Plans</p>
+          <p class="text-justify">
+            Baseline and upgrade plans must be provided for every job. These can be hand drawn and photographed to be submitted with the rest of the evidence.
+          </p>
+
+        </div>
+      </div>
+      <br>
+      <br>
+    </div>
+  </div>
+<br>
+  <div class="row">
+    <div class="col-md-offset-1 col-md-10" style="border-style:outset;">
+      <br>
+      <div class="row">
+        <div class="col-sm-offset-1 col-md-10">
+          <p class="text-justify">
+            Graph paper has been included in the document pack for this Purpose.
+          </p>
+          <p class="text-justify">
+            We are flexible on how the plans are drawn if the data is accurate clearly understood. Once the hand drawn plans are submitted to us, we digitise the drawings using floor plan software and lighting design simulations are then conducted on the computer-generated plan. AS1680 lighting standards are assessed against the output.
+<br>The key details we need in the plan are:
+          </p>
+          <ul>
+            <li>
+              <p>Shape and dimensions of the space</p>
+            </li>
+            <li>
+              <p>Baseline lights in their existing locations</p>
+            </li>
+            <li>
+              <p>Ceiling or luminaire mounting heights</p>
+            </li>
+            <li>
+              <p>Distinguishing features of the building like roller doors and entries. This helps us orientate our selves in the space when referring to the evidence photos.</p>
+            </li>
+            <li>
+              <p>Any changes to the layout/ new luminaire locations</p>
+            </li>
+            <li>
+              <p>Location of air conditioning vents if any</p>
+            </li>
+          </ul>
+          <p class="bold">Submission to ai Lighting.</p>
+          <p class="text-justify">
+            Every section included in the document pack can be photographed individually and uploaded to a shared folder via the Google Photos application. Photos of the supporting documents like Customer Invoice, CES, and sign off form can be photographed off site at your home or office. The geo tagging of these photos are not important. Only lamp/ control gear and space photos require GPS data that match the installation address.
+          </p>
+          <p class="text-justify">
+            Google Photos is a universal application that provides a simple way to share your project documents reliably without damaging the meta-data contained within the mandatory site photos.
+          </p>
+          <p class="text-justify">
+            To share your photos with ai Lighting you must have a Google account. If you don’t have a Google account, you can create one by clicking this link -> <a target="_blank" href="https://accounts.google.com/signup/v2/webcreateaccount?continue=https%3A%2F%2Fplay.google.com%2Fstore%2Fapps%2Fdetails%3Fid%3Dcom.google.android.apps.photos%26referrer%3Dutm_source%253Dphotosweb%2526utm_medium%253Dpromo%2526utm_term%253Dempty&flowName=GlifWebSignIn&flowEntry=SignUp"><img src="<?= base_url('asset/img/google.png') ?>" style="height:20px" alt=""></a>.
+          </p>
+          <p class="text-justify">
+            The Google photos is self-explanatory and easy to use. Select the photos you wish to upload and share them with the ai.lighting.photos@gmail.com account. If you are having trouble with this step, get in touch and we will talk you through it.
+          </p>
+          <p class="text-justify">
+            You can download the google photos app for your mobile device by clicking either of the links below.
+          </p>
+          <img src="<?= base_url('asset/img/download.png') ?>" style="height:70px" alt="">
+          <br>
+          <a target="_blank" href="https://play.google.com/store/apps/details?id=com.google.android.apps.photos&hl=en"><img src="<?= base_url('asset/img/google_play.png') ?>" style="height:64px" alt=""></a>
+          <a target="_blank" href="https://itunes.apple.com/us/app/google-photos/id962194608?mt=8"><img src="<?= base_url('asset/img/appstore.png') ?>" style="height:43px" alt=""></a>
+          <br>
+          <p class="text-justify">
+            Once a project is approved by ai Lighting to commence there are a few steps that need to occur to have lighting dispatched to the project.
+          </p>
+        </div>
+      </div>
+      <br>
+      <br>
+    </div>
+  </div>
+<br>
+  <div class="row">
+    <div class="col-md-offset-1 col-md-10" style="border-style:outset;">
+      <br>
+      <div class="row">
+        <div class="col-sm-offset-1 col-md-10">
+          <p class="text-justify">
+            Ai Lighting reserves the right to reject any project if the order does not pass an internal risk assessment.
+          </p>
+          <p class="text-justify">
+            Ai Lighting is effectively granting credit to projects. To minimise the financial risk the Installing Electrician and the Customer must co-sign a Purchase order / Project authorisation document that accepts:
+          </p>
+          <ul>
+            <li>
+              <p>The job is authorised to proceed</p>
+            </li>
+            <li>
+              <p>The customer/ energy consumer understands the scope of works that is occurring.</p>
+            </li>
+            <li>
+              <p>Financial liability of the stock if the project is not completed and VEECS are not
+created. Or,</p>
+            </li>
+            <li>
+              <p>Grants ai Lighting permission take control of completing a project on behalf of the
+energy consumer if the installing electrician fails to meet timeframe and installation
+performance.</p>
+            </li>
+            <li>
+              <p>Grants ai Lighting permission to collect stock that isn’t installed</p>
+            </li>
+          </ul>
+
+          <p class="text-justify">It is in the best interest of all parties that any problem is resolved before any action is taken. The document protects against criminal activity and ultimately protects the energy consumer.</p>
+          <p class="text-justify">Depending on the size and location of the job dispatch method of materials will be determined case by case.</p>
+          <br>
+          <p class="bold">Drop off/ Collection of the removed lighting.</p>
+          <p class="text-justify">Every single lamp and ballast must be retained for recycling. Ai Lighting will work with you to take possession of the old lighting. Depending on the size of the job, location and how busy we are, we may be able to collect the old lighting. We will work with you to get the lighting to our Narre Warren Location where it will be processed at the Hampton Park Transfer Station.</p>
+          <p class="text-justify">
+            If the number of returned lights does not match the claim, the claim will be reduced to match the recycling evidence. Ai Lighting takes care of this step and ensure the spent lighting is disposed off responsibly and the correct documentation is collected in the form of an itemised recycling receipt. A claim cannot be processed without this evidence.
+          </p>
+          <br>
+          <p class="bold">
+            Processing Time and Payments.
+          </p>
+          <p class="text-justify">
+            Processing times vary. There is lots of work in the background that occurs to create the VEECS that eventually turn into revenue that pays for your work. Claims are processed in the order that they are received. You will be advised prior to commencement of the job if there is any extended lead time on processing. Generally, the claim is processed within a few days of receiving it. If there is information missing you will be sent a RFI to complete. The sooner we get the information the sooner we get the claim in. If documentation is not received within a timely manner it can increase the risk of VEEC rise and fall and the estimated amount of money you earn may change. Once a claim has been approved, a Purchase order will be issued to you with the final amount. Once invoiced, it will be paid between 5-10 business days.
+          </p>
+        </div>
+      </div>
+      <br>
+      <br>
+    </div>
+  </div>
+  <br>
+
+  <div class="row">
+    <div class="col-md-offset-1 col-md-10" style="border-style:outset;">
+      <br>
+          <img src="<?= base_url('asset/img/background05.jpg') ?>" alt="" class="img-responsive">
+    </div>
+  </div>
+  <br>
+
+  <div class="row">
+    <div class="col-md-offset-1 col-md-10" style="border-style:outset;">
+      <br>
+          <img src="<?= base_url('asset/img/background06.jpg') ?>" alt="" class="img-responsive">
+    </div>
+  </div>
+  <br>
+
+  <div class="row">
+    <div class="col-md-offset-1 col-md-10" style="border-style:outset;">
+      <br>
+          <img src="<?= base_url('asset/img/background07.jpg') ?>" alt="" class="img-responsive">
+    </div>
+  </div>
+  <br>
+
+  <div class="row">
+    <div class="col-md-offset-1 col-md-10" style="border-style:outset;">
+      <br>
+          <img src="<?= base_url('asset/img/background08.jpg') ?>" alt="" class="img-responsive">
+    </div>
+  </div>
+  <br>
+
+  <div class="row">
+    <div class="col-md-offset-1 col-md-10" style="border-style:outset;">
+      <br>
+          <img src="<?= base_url('asset/img/background09.jpg') ?>" alt="" class="img-responsive">
+    </div>
+  </div>
+  <br>
+
+  <div class="row">
+    <div class="col-md-offset-1 col-md-10" style="border-style:outset;">
+      <br>
+          <img src="<?= base_url('asset/img/background10.jpg') ?>" alt="" class="img-responsive">
+    </div>
+  </div>
+  <br>
+
+  <div class="row">
+    <div class="col-md-offset-1 col-md-10" style="border-style:outset;">
+      <br>
+          <img src="<?= base_url('asset/img/background11.jpg') ?>" alt="" class="img-responsive">
+    </div>
+  </div>
+  <br>
+
+  <div class="row">
+    <div class="col-md-offset-1 col-md-10" style="border-style:outset;">
+      <br>
+          <img src="<?= base_url('asset/img/background12.jpg') ?>" alt="" class="img-responsive">
+    </div>
+  </div>
+  <br>
+
+  <div class="row">
+    <div class="col-md-offset-1 col-md-10" style="border-style:outset;">
+      <br>
+          <img src="<?= base_url('asset/img/background13.jpg') ?>" alt="" class="img-responsive">
+    </div>
+  </div>
+  <br>
+
+  <div class="row">
+    <div class="col-md-offset-1 col-md-10" style="border-style:outset;">
+      <br>
+          <img src="<?= base_url('asset/img/background14.jpg') ?>" alt="" class="img-responsive">
+    </div>
+  </div>
+  <br>
+
+  <div class="row">
+    <div class="col-md-offset-1 col-md-10" style="border-style:outset;">
+      <br>
+          <img src="<?= base_url('asset/img/background15.jpg') ?>" alt="" class="img-responsive">
+    </div>
+  </div>
+  <br>
+
+  <br>
+  
 
 </div>
