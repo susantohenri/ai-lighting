aa49079a21f4b62ef47780439ee33d8f172bd39b
ari yulianto <sadhean@gmail.com>
2018-07-16 20:14:52 +0700

    basic form on project appliaction form

--- application/views/Project.php
+++ application/views/Project.php
@@ -2,10 +2,18 @@
   <div class="row">
     <div class="col-sm-12">
         <h3 class="text-center" style="margin:5px 0px;"><strong>Project Application</strong></h3> <br>
-        </div>
     </div>
-  </div>
+    <div class="col-sm-12 text-center" >
 
+      <p class="bold">Fill in the details bellow regarding your prospective upgrade project and an aiLighting electrician will be in contact shortly</p>
+      <hr class="bold">
+      <br>
+    </div>
+
+  </div>
+  <br>
+
+<!--
   <div class="row">
     <div class="col-sm-12">
       <div id="wufoo-z1uq74zf04yi48z">
@@ -28,6 +36,92 @@ var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') retu
 try { z1uq74zf04yi48z = new WufooForm();z1uq74zf04yi48z.initialize(options);z1uq74zf04yi48z.display(); } catch (e) {}};
 var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
 })(document, 'script');</script>
+    </div>
+  </div>
+-->
+
+  <div class="">
+    <div class="col-sm-offset-1 col-sm-10">
+      <form role="form">
+        <div class="form-group">
+          <label for="company" class="bold">Your Company Name *</label>
+          <input type="input" class="form-control" id="company" name="company" placeholder="Company Name">
+        </div>
+        <br>
+
+        <div class="form-inline">
+          <label for="" class="bold">Company Contact *</label> <br>
+          <div class="form-group">
+            <input type="input" class="form-control" id="first_name" name="first_name" placeholder="First Name">
+          </div>
+          <div class="form-group">
+            <input type="input" class="form-control" id="last_name" name="last_name" placeholder="Last Name">
+          </div>
+        </div>
+        <br>
+        <div class="form-group">
+          <label for="phone" class="bold">Phone Number *</label>
+          <input type="input" class="form-control" id="phone" name="phone" placeholder="Phone Number">
+        </div>
+        <br>
+        <div class="form-group">
+          <label for="email" class="bold">Email *</label>
+          <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+        </div>
+        <br>
+        <div class="form-group">
+          <label for="suburb" class="bold">Project Suburb *</label>
+          <input type="input" class="form-control" id="suburb" name="suburb" placeholder="Project Suburb">
+        </div>
+        <br>
+        
+        <label for="" class="bold">Main purpose of the project building *</label>
+        <div class="radio">
+          <label>
+            <input type="radio" name="purpose" id="purpose1" value="Storage"> Storage
+          </label>
+        </div>
+        <div class="radio">
+          <label>
+            <input type="radio" name="purpose" id="purpose2" value="Sanufacturing"> Manufacturing
+          </label>
+        </div>
+        <div class="radio">
+          <label>
+            <input type="radio" name="purpose" id="purpose3" value="Automative"> Automotive related 
+          </label>
+        </div>
+        <div class="radio">
+          <label>
+            <input type="radio" name="purpose" id="purpose4" value="Retail"> Retail 
+          </label>
+        </div>
+          <br>
+        <div class="form-group">
+          <label for="highbays" class="bold">Number Of Highbays</label>
+          <input type="input" class="form-control" id="highbays" name="highbays">
+        </div>
+        <br>
+
+        <label for="" class="bold">Exisiting Highbay Wattage</label>
+        <div class="radio">
+          <label>
+            <input type="radio" name="watt" id="watt1" value="400"> 400 W
+          </label>
+        </div>
+        <div class="radio">
+          <label>
+            <input type="radio" name="watt" id="watt2" value="250"> 250 W
+          </label>
+        </div>
+        <div class="radio">
+          <label>
+            <input type="radio" name="watt" id="watt3" value="other"> Not Sure / Other 
+          </label>
+        </div>
+        <br>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
     </div>
   </div>
 </div>
